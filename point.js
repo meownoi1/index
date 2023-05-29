3957c4cd68050b183dc56a2c141c88b4fab67fb2
meownoi1 <124670798+meownoi1@users.noreply.github.com>
2023-05-29 10:55:33 +0700

    Add files via upload

---
+++ point.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ครัวอัปษร",
+          "type": "พิเศษ",
+          "level": 4.3
+        },
+        "geometry": {
+          "coordinates": [
+            100.4504662,
+            13.7309454
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Supanniga Eating Room",
+          "type": "ธรรมชาติ",
+          "level": 4.3
+        },
+        "geometry": {
+          "coordinates": [
+            100.3706384,
+            13.7309317
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เรือนมัลลิการ์",
+          "type": "วัฒนธรรม",
+          "level": 4.2
+        },
+        "geometry": {
+          "coordinates": [
+            100.5605986,
+            13.7251707
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เมธาวลัย ศรแดง",
+          "type": "วัฒนธรรม",
+          "level": 4.3
+        },
+        "geometry": {
+          "coordinates": [
+           100.5023946,
+           13.7461094
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "กินอยู่ได้",
+          "type": "วัฒนธรรม",
+          "level": 5.0
+        },
+        "geometry": {
+          "coordinates": [
+            100.4859733,
+            13.8793334
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อรรถรส",
+          "type": "ธรรมชาติ",
+          "level": 4.2
+        },
+        "geometry": {
+          "coordinates": [
+            100.4714943,
+            13.7460989
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ลูกไก่ทอง",
+          "type": "วัฒนธรรม",
+          "level": 3.9
+        },
+        "geometry": {
+          "coordinates": [
+            100.4380237,
+            13.726138
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Sala Rattanakosin",
+          "type": "วัฒนธรรม",
+          "level": 4.2
+        },
+        "geometry": {
+          "coordinates": [
+           100.4883367,
+           13.7452707
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ราก (RARK Authentic Thai Cuisine)",
+          "type": "วัฒนธรรม",
+          "level": 4.7
+        },
+        "geometry": {
+          "coordinates": [
+            100.5414676,
+            13.6798851
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เขียวไข่กา",
+          "type": "วัฒนธรรม",
+          "level": 4.2
+        },
+        "geometry": {
+          "coordinates": [
+            100.517997,
+            13.7942141
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
